558bb9fe665fe6d739a2a39037de795da1bd6e4b
AlecLozano <lozanoalec46@gmail.com>
2024-09-12 19:01:58 -0400

    Fixed Syntax

--- RaspberryPi/serialCom.py
+++ RaspberryPi/serialCom.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.JOYBUTTONUP or  event.type == pygame.JOYBUTTONDOWN:
                 value = int(controller.get_button(event.button))
-                arduino.write(struct.pack("ii", event.button, value)
+                arduino.write(struct.pack("ii", event.button, value))
 
             if event.type == pygame.JOYAXISMOTION:
                 axisValue = controller.get_axis(event.axis)
@@ -25,12 +25,29 @@ if __name__ == '__main__':
             if event.type == pygame.JOYHATMOTION:
                 value = 0
                 
-                match controller.get_hat(event.hat) {
-                    case (0, 1): value = 0
-                    case (-1, 0): value = 1
-                    case (0, -1): value = 2
-                    case (1, 0): value = 3
-                }
+                # match controller.get_hat(event.hat) {
+                #     case (0, 1): value = 0
+                #     case (-1, 0): value = 1
+                #     case (0, -1): value = 2
+                #     case (1, 0): value = 3
+                # }
 
-                arduino.write(struct.pack("ii", 21, value)
+                hatValue = controller.get_hat(event.hat)
+
+                if hatValue == (0,0):
+                    value = 0
+
+                if hatValue == (0,1):
+                    value = 1
+
+                if hatValue == (-1,0):
+                    value = 2
+
+                if hatValue == (0,-1):
+                    value = 3
+                    
+                if hatValue == (1,0):
+                    value = 4 
+
+                arduino.write(struct.pack("ii", 21, value))
 
