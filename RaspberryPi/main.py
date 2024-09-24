fd1ffabf60dfa34de049a4172d5450bd20df1497
Aaron <millhouseaa@gmail.com>
2024-09-24 19:21:09 -0400

    Should probably actually add those changes

---
+++ RaspberryPi/main.py
@@ -0,0 +1,70 @@
+# pip libs
+import serial
+import pygame
+import sys
+import serial.serialutil
+
+# built-in libs
+import time
+import asyncio
+import logging
+import subprocess
+import os
+
+# internal files
+from XboxController.XboxController import XboxController
+
+script_dir = os.path.dirname(__file__)
+subprocess.Popen([sys.executable, "camera/stream.py"], cwd=script_dir)
+
+async def main():
+    # Connect to the Arduino
+    connected = False
+    while not connected:
+        try:
+            arduino = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
+            arduino.reset_input_buffer()
+            arduino.reset_output_buffer()
+            connected = True
+        except serial.serialutil.SerialException:
+            logging.error("Couldn't connect to Arduino")
+            time.sleep(1)
+
+    connected = False
+
+    # Connect to the Xbox Controller
+    while not connected:
+        try:
+            pygame.init()
+            controller = XboxController(deadZone=0.25)
+            connected = True
+        except pygame.error:
+            logging.error("Couldn't connect to controller")
+            pygame.joystick.quit()
+        time.sleep(1)
+
+    logging.info("Starting Testing")
+
+    # Tests Serial Communication
+    while arduino.in_waiting == 0:
+        arduino.write("Testing\n".encode("utf-8"))
+        time.sleep(1)
+
+    print(arduino.readline().decode("utf-8").rstrip())
+    logging.info("Ending Testing")
+    while True:
+        # Detects and sends controller inputs
+        instructions = None
+        instructions = await controller.getControllerInput()
+
+        if instructions != None:
+            for instruction in instructions:
+                arduino.write(instruction.encode("utf-8"))
+
+        # Prints string in the buffer
+        if arduino.in_waiting:
+            response = arduino.readline().decode("utf-8").rstrip()
+            print(response)
+
+
+asyncio.run(main())
